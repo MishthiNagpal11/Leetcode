b57a5f43d1ff0b9b346f591804fa748945b59daa
Mishthi Nagpal <Mishthi.12906@stu.upes.ac.in>
2026-08-25 10:18:24 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (90.41%) - LeetSync

---
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+        int i=1;
+        while(true){
+            int multiple= k*i;
+            boolean found=false;
+        for(int j=0;j<nums.length;j++){
+            if(nums[j]==multiple){
+                found=true;
+                break;
+            }
+        }
+        if(!found){
+            return multiple;
+
+        }
+        i++;
+        }
+    }
+}
