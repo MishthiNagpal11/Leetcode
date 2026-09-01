047b5f6e8565d70af0c7de1fa63eb0568266bba5
Mishthi Nagpal <Mishthi.12906@stu.upes.ac.in>
2026-09-01 19:28:12 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (32.42%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,26 +1,11 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-
-        if (strs == null || strs.length == 0) {
-            return "";
-        }
-
-        String ans = "";
-
-        for (int j = 0; j < strs[0].length(); j++) {
-
-            char current = strs[0].charAt(j);
-
-            for (int i = 1; i < strs.length; i++) {
-
-                if (j >= strs[i].length() || strs[i].charAt(j) != current) {
-                    return ans;
-                }
+        String prefix= strs[0];
+        for(int i=0;i<strs.length;i++){
+            while(!strs[i].startsWith(prefix)){
+                prefix=prefix.substring(0,prefix.length()-1);
             }
-
-            ans = ans + current;
         }
-
-        return ans;
+        return prefix;
     }
 }
